016a367c196f0fd29e6a29925a5406351eb54351
= <=>
2024-06-15 02:52:38 +0200

    Aceept requests

--- public/requests/requst.js
+++ public/requests/requst.js
@@ -1,67 +1,101 @@
 async function getData() {
-    console.log("running");
-    try {
-        const myUsername = localStorage.getItem("username");
-        let req = [];
-        const querySnapshot = await db.collection("chatInvite").get();
+  console.log("running");
+  try {
+    const myUsername = localStorage.getItem("username");
+    let req = [];
+    let ids = [];
+    const querySnapshot = await db.collection("chatInvite").get();
 
-        querySnapshot.forEach((doc) => {
-            const userData = doc.data();
-            if (userData.to === myUsername) {
-                req.push(userData);
-            }
-            console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+    querySnapshot.forEach((doc) => {
+      const userData = doc.data();
+      if (userData.to === myUsername) {
+        req.push(userData);
+        ids.push(doc.id);
+      }
+      console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+    });
+
+    // Check if there are requests to display
+    if (req.length > 0) {
+      const cardContainer = document.querySelector(".card__content"); // Assuming you have a container for cards
+
+      // Loop through each request data
+      req.forEach((userData) => {
+        // Create card elements
+        const card = document.createElement("div");
+        card.classList.add("card"); // Add class 'card'
+
+        const img = document.createElement("img");
+        img.src = "../asserts/3135715.png";
+        img.style.width = "45px";
+        img.style.height = "45px";
+
+        const label = document.createElement("label");
+        label.id = "personName";
+        label.textContent = `${userData.from} has requested to chat with you`;
+
+        const buttonAccept = document.createElement("button");
+        buttonAccept.id = "accept";
+        buttonAccept.textContent = "Accept";
+
+        buttonAccept.addEventListener("click", () => {
+            console.log("accepted");
+          ids.forEach((id) => {
+            console.log(id); 
+            //update the status   
+            update(id);
+            //create a message
+            createfirstMessage(userData.from);  
+           
+          });
         });
 
-        // Check if there are requests to display
-        if (req.length > 0) {
-            const cardContainer = document.querySelector('.card__content'); // Assuming you have a container for cards
-
-            // Loop through each request data
-            req.forEach((userData) => {
-                // Create card elements
-                const card = document.createElement('div');
-                card.classList.add('card'); // Add class 'card'
-
-                const img = document.createElement('img');
-                img.src = "../asserts/3135715.png";
-                img.style.width = "45px";
-                img.style.height = "45px";
-
-                const label = document.createElement('label');
-                label.id = "personName";
-                label.textContent = `${userData.from} has requested to chat with you`;
-
-                const buttonAccept = document.createElement('button');
-                buttonAccept.id = "accept";
-                buttonAccept.textContent = "Accept";
-
-                buttonAccept.addEventListener("click", ()=>{
-                    //delete the entry
-                    //create a new message between the 2
-                    
-
-                })
-
-                const buttonReject = document.createElement('button');
-                buttonReject.id = "reject";
-                buttonReject.textContent = "Reject";
-
-                // Append elements to card
-                card.appendChild(img);
-                card.appendChild(label);
-                card.appendChild(buttonAccept);
-                card.appendChild(buttonReject);
-
-                // Append card to container
-                cardContainer.appendChild(card);
-            });
+        async function update(id) {
+          try {
+            const docRef = await db
+              .collection("chatInvite")
+              .doc(id)
+              .update({
+                Accepted: true
+              });
+            console.log("successfully updated ");
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
         }
 
-    } catch (e) {
-        console.error("Error getting data: ", e);
-    }
-}
+        async function createfirstMessage(to) {
+            try {
+                const docRef = await db.collection("Message").add({
+                  From : localStorage.getItem("username"),
+                  To : to,
+                  Message : "chat request accepted",
+                  time : "Jan 01 0000, 00:00:00"
+                });
+                console.log("elements inserted successfully ", docRef.id);
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
+        }
 
+
+        const buttonReject = document.createElement("button");
+        buttonReject.id = "reject";
+        buttonReject.textContent = "Reject";
+
+        // Append elements to card
+        card.appendChild(img);
+        card.appendChild(label);
+        card.appendChild(buttonAccept);
+        card.appendChild(buttonReject);
+
+        // Append card to container
+        cardContainer.appendChild(card);
+      });
+    }
+  } catch (e) {
+    console.error("Error getting data: ", e);
+  }
+}
 
 getData();
