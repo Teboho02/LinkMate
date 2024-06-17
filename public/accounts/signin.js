cff9e0209067dabf74b3a158ddf1a1dca1211f63
= <=>
2024-06-17 21:25:34 +0200

    update manifest

--- public/accounts/signin.js
+++ public/accounts/signin.js
@@ -41,15 +41,15 @@ si.addEventListener("click", async () => {
     }
 });
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    console.log(
-    "Geolocation is not supported by this browser.");
-  }
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     console.log(
+//     "Geolocation is not supported by this browser.");
+//   }
   
-  function showPosition(position) {
-    console.log(
-    "Latitude: " + position.coords.latitude +
-    "Longitude: " + position.coords.longitude);
-  }
+//   function showPosition(position) {
+//     console.log(
+//     "Latitude: " + position.coords.latitude +
+//     "Longitude: " + position.coords.longitude);
+//   }
