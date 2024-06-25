63573742668622ee8138a741b8451c87cb00b357
= <=>
2024-06-26 00:07:44 +0200

    try express

--- public/api/users/route.js
+++ public/api/users/route.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const app = express();
 
-{
-    text: "HELLO WORLD"
-};
+app.get('/api/users/login', (req, res) => {
+  const a = {
+    text: "HELLO WORLD",
+    age: "20"
+  };
+
+  res.json(a);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
