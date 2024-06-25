73d7b81f5f127bc5d09b0a76389364097e886db8
= <=>
2024-06-26 01:15:45 +0200

    change to express

--- public/accounts/signin.js
+++ public/accounts/signin.js
@@ -1,7 +1,6 @@
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 const si = document.getElementById("sii");
-import config from "./api/users/config.js";
 
 if(localStorage.getItem("username") != null){
    //TODOs uncomment later
