e9de2fe8d2eb46af7aaa5524b30b00a1f3f4a5b1
= <=>
2024-06-25 23:57:46 +0200

    stars

--- public/api/users/login.js
+++ public/api/users/login.js
@@ -1,14 +1,5 @@
-//const config = require("../config.js");
 
-export async function GET(req) {
   const a = {
     text: "HELLO WORLD",
     age: "20"
   };
-
-  console.log(typeof a);
-
-  return new Response(JSON.stringify(a), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
