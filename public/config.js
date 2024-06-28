627d07054bdcbc2eda16de169bbfb3658a45b8a7
= <=>
2024-06-29 01:51:27 +0200

    update APIs

--- public/config.js
+++ public/config.js
@@ -1,10 +1,9 @@
 // Your Firebase configuration
 //require('dotenv').config();
 
-require('dotenv').config();
 
 const firebaseConfig = {
-  apiKey: process.env.API_URL,
+  apiKey: "AIzaSyDFBwd8yFSrkKvZikexNWb8VMRozAe1mEk",
   authDomain: "find-my-patner.firebaseapp.com",
   projectId: "find-my-patner",
   storageBucket: "find-my-patner.appspot.com",
