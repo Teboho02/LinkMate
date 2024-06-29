602c130ba27c5450f59d2e9cd24a1cee1c32f77c
= <=>
2024-06-29 22:42:09 +0200

    update the DOM

--- public/home/refresh.js
+++ public/home/refresh.js
@@ -10,7 +10,7 @@ async function checkRequests() {
     let count = 0;
 
     console.log("running");
-    const res = await db.collection("chatInvite");
+    const res = await db.collection("chatInvite").where("to", "=", "Test3");
 
     const querySnapshot = await res.get();
     querySnapshot.forEach((doc) => {
