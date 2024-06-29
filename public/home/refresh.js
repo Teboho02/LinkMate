fd7c3b9d4bfbae166cf13ae9d76dd66277ca0575
= <=>
2024-06-29 22:24:36 +0200

    check and display messahes

--- public/home/refresh.js
+++ public/home/refresh.js
@@ -7,18 +7,25 @@ const myUsername = localStorage.getItem("username");
 checkRequests();
 async function checkRequests() {
 
+    let count = 0;
+
     console.log("running");
-    const res = await db.collection("chatInvite").where("to","=",myUsername);
+    const res = await db.collection("chatInvite");
 
     const querySnapshot = await res.get();
     querySnapshot.forEach((doc) => {
-
         const userdata = doc.data();
         console.log(userdata.to);
         console.log(userdata.from);
 
+        if(!userdata.Accepted && userdata.to === myUsername){
+            count++;
+        }
+
 
     });
+
+    alert("count ",count);
 
 
 }
