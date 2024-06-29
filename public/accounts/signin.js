f499794caaa1f02638681d3391495ca73425ec46
= <=>
2024-06-30 01:55:00 +0200

    add message

--- public/accounts/signin.js
+++ public/accounts/signin.js
@@ -9,6 +9,19 @@ if(localStorage.getItem("username") != null){
 
 
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDFBwd8yFSrkKvZikexNWb8VMRozAe1mEk",
+    authDomain: "find-my-patner.firebaseapp.com",
+    projectId: "find-my-patner",
+    storageBucket: "find-my-patner.appspot.com",
+    messagingSenderId: "892330665624",
+    appId: "1:892330665624:web:1d597128bb74982e9a52ed"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  
 
 
 async function getData(username, password) {
