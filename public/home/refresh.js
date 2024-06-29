f4e4b409e52938b0e151c9054317443eb7f09f62
= <=>
2024-06-29 23:07:29 +0200

    update the DOM

--- public/home/refresh.js
+++ public/home/refresh.js
@@ -1,6 +1,5 @@
 
 
-//check for new chat request. If available, update the DO, show 1
 const reqMenu = document.getElementById("chatReq");
 const myUsername = localStorage.getItem("username");
 
@@ -8,35 +7,35 @@ checkRequests();
 async function checkRequests() {
 
     let count = 0;
-
-    console.log("running");
     const res = await db.collection("chatInvite").where("to", "==", myUsername).where("Accepted" , "==" , false);
 
     const querySnapshot = await res.get();
     querySnapshot.forEach((doc) => {
-        const userdata = doc.data();
-
-        if(!userdata.Accepted && userdata.to === myUsername){
-            count++;
-        }
-
-
+            count++;    
     });
-
-
     if(count > 0){
         var chatReqElement = document.getElementById('chatReq').querySelector('a');
         var icon = chatReqElement.querySelector('i');
         chatReqElement.textContent = ' Chat Requests(' + count + ')';
         chatReqElement.prepend(icon); 
     }
-
-    //change inner html of chat request
-
-
-
 }
 
-function checkMessages() {
-    //diplay the number of people
+async function checkMessages() {
+    try {
+        let unreadCount = 0;
+        const res = await db.collection("Message").where("To", "==", myUsername);
+
+        const querySnapshot = await res.get();
+        querySnapshot.forEach((doc) => {
+            unreadCount++;
+        });
+
+        alert("unread ",unreadCount);
+        if (unreadCount > 0) {
+            console.log(`You have ${unreadCount} unread messages.`);
+        }
+    } catch (error) {
+        console.error("Error checking messages:", error);
+    }
 }
--- public/refresh.js
+++ public/refresh.js
@@ -1,5 +1,31 @@
 
-//check for new messages or chat requests.
 
 
 
+checkRequests();
+async function checkRequests() {
+
+    let count = 0;
+    const res = await db.collection("chatInvite").where("to", "==", myUsername).where("Accepted" , "==" , false);
+
+    const querySnapshot = await res.get();
+    querySnapshot.forEach((doc) => {
+            count++;    
+    });
+    if(count > 0){
+       sessionStorage.setItem("requests", count);
+    }
+}
+
+async function checkMessages() {
+
+    let read = 0;
+    const res = await db.collection("Message").where("To", "==", myUsername).where("read" , "==" , false);
+
+    const querySnapshot = await res.get();
+    querySnapshot.forEach((doc) => {
+        const userdata = doc.data();
+        
+    });
+
+}
