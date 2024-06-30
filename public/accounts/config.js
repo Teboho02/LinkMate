10ad0258a20d679d343008d964902ec9d4a80e06
= <=>
2024-06-30 02:06:43 +0200

    secure my keys

--- public/accounts/config.js
+++ public/accounts/config.js
@@ -1,13 +1,23 @@
+
+async function fetchEnv() {
+  const response = await fetch('/api/getEnv');
+  const data = await response.json();
+  console.log(data);
+
+  return data.apiUrl
+
+}
+
+const key = fetchEnv();
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDFBwd8yFSrkKvZikexNWb8VMRozAe1mEk",
-    authDomain: "find-my-patner.firebaseapp.com",
-    projectId: "find-my-patner",
-    storageBucket: "find-my-patner.appspot.com",
-    messagingSenderId: "892330665624",
-    appId: "1:892330665624:web:1d597128bb74982e9a52ed"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-  
+  apiKey: key,
+  authDomain: "find-my-patner.firebaseapp.com",
+  projectId: "find-my-patner",
+  storageBucket: "find-my-patner.appspot.com",
+  messagingSenderId: "892330665624",
+  appId: "1:892330665624:web:1d597128bb74982e9a52ed"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
