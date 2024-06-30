d0ae777ae62cffa6f17ba8831bcb2be6d900cfbb
= <=>
2024-06-30 02:16:33 +0200

    secure my keys

--- server.js
+++ server.js
@@ -2,21 +2,18 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 
-// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// API endpoint to get environment variables
 app.get('/api/getEnv', (req, res) => {
   res.json({ apiUrl: process.env.FIREBASE_PRIVATE_KEY });
 });
 
-// Catch-all handler to serve index.html for any route not handled above
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
