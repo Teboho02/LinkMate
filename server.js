7f98de9bb4183bd24ec66bdc5028db7cec59d0be
= <=>
2024-06-30 19:53:54 +0200

    intergrate gemini

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
-
-
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.googleAPI);
-
-async function run() {
-  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
-  const prompt = "Write a story about a magic backpack."
-
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
-
-run();
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const genAI = new GoogleGenerativeAI(process.env.googleAPI);
+
+async function generateStory() {
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const prompt = "Write a story about a magic backpack.";
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = await response.text();
+    return text;
+  } catch (error) {
+    console.error("Error generating story:", error);
+    return "Error generating story.";
+  }
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,8 +30,10 @@ app.get('/api/getEnv', (req, res) => {
   res.json({ apiUrl: process.env.FIREBASE_PRIVATE_KEY });
 });
 
-
-  
+app.get('/gemini', async (req, res) => {
+  const story = await generateStory();
+  res.send(story);
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -43,19 +42,3 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// function encryptData(plainText, secretKey) {
-//   const encryptedData = CryptoJS.AES.encrypt(plainText, secretKey).toString();
-//   return encryptedData;
-// }
-
-// function decryptData(encryptedData, secretKey) {
-//   const bytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
-//   const decryptedText = bytes.toString(CryptoJS.enc.Utf8);
-//   return decryptedText;
-// }
-
-
-
-
-
