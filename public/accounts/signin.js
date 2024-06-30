bc535835232abaadc5576711bd2f43f99d40d489
= <=>
2024-06-30 02:20:24 +0200

    check if profile has alredy been filled

--- public/accounts/signin.js
+++ public/accounts/signin.js
@@ -2,9 +2,9 @@ const username = document.getElementById("username");
 const password = document.getElementById("password");
 const si = document.getElementById("sii");
 
-if(localStorage.getItem("username") != null){
-   //TODOs uncomment later
-   // window.location.href = "./profile/profile.html"
+if (localStorage.getItem("username") != null) {
+    //TODOs uncomment later
+    // window.location.href = "./profile/profile.html"
 }
 
 
@@ -17,8 +17,8 @@ async function getData(username, password) {
 
     const userDocSnap = await userDocRef.get();
 
-        return userDocSnap.data().password == password ? true : false;
-      
+    return userDocSnap.data().password == password ? true : false;
+
 }
 
 si.addEventListener("click", async () => {
@@ -26,8 +26,30 @@ si.addEventListener("click", async () => {
     const result = await getData(username.value, password.value);
 
     if (result) {
-        localStorage.setItem("username",username.value);
-        window.location.href = "./profile/profile.html";
+        localStorage.setItem("username", username.value);
+
+        //check if profile is already filled
+
+        const username = username.value; // Replace with the actual username
+
+        const userDocRef = db.collection("Profile").doc(username);
+
+        userDocRef.get()
+            .then((doc) => {
+                if (doc.exists) {
+                    console.log("Document data:", doc.data());
+                } else {
+                    console.log("No such document!");
+                    window.location.href = "./profile/profile.html";
+
+                }
+            })
+            .catch((error) => {
+                console.error("Error getting document:", error);
+            });
+
+
+
 
     } else {
         alert("Invalid username or password");
