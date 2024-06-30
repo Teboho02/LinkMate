4568680657405cb10db65ee73df051e2729daf59
= <=>
2024-06-30 16:41:43 +0200

    change time format

--- public/requests/requst.js
+++ public/requests/requst.js
@@ -100,7 +100,7 @@ async function createFirstMessage(to) {
       From: localStorage.getItem("username"),
       To: to,
       message: "Chat request accepted",
-      time: [fetchCurrentTime()] 
+      time: ["10"] 
     });
     console.log("Message created successfully");
   } catch (e) {
